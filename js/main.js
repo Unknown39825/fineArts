1f3918e84e683bd5abb54a5296ebf03fcd1f2756
Unknown39825 <61468014+Unknown39825@users.noreply.github.com>
2020-03-28 22:52:31 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const res ={
+  0:
+  {items:1},
+  320:
+  {items: 1},
+  560:
+  {items: 2},
+  960:
+  {items:3}
+}
+
+
+function hello() 
+{
+    var x = document.getElementById("hell");
+    if (x.className == "nav") 
+    x.className += " collapse";
+    else
+      x.className = "nav";
+}
+
+$(document).ready(function () {
+
+
+  $('.owl-carousel').owlCarousel(
+    {
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      dots:false,
+      nav:true,
+      responsive:res,
+      navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+      
+    }
+  );
+
+  AOS.init();
+
+
+
+
+});
+
