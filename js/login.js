ba95264483cccc73ea9046904d0353d21bcea151
Unknown39825 <61468014+Unknown39825@users.noreply.github.com>
2020-05-13 13:27:29 +0530

    login page added

---
+++ js/login.js
@@ -0,0 +1,25 @@
+var attempt = 3;
+function showlogin()
+{
+    document.getElementById('loginp').style.display = "block";
+
+}
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "admin" && password == "admin") {
+        alert("Login successfully");
+        document.getElementById('loginp').style.display="none";
+        return false;
+    }
+    else {
+        attempt--;
+        alert("You have left " + attempt + " attempt;");
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
